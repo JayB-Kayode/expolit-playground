f26832458dc5e07df4d267479be64a8f091d2878
jean-brice <jdickewu@gmail.com>
2017-09-15 16:25:33 +0000

    add new code to the files to install refs #7

--- Phishing/setup everything Debian.sh
+++ Phishing/setup everything Debian.sh
@@ -1,6 +1,9 @@
 #! bin/bash 
 
-
+echo "don't forget to run as root"
 #update 
 apt-get update 
 apt-get install apache2
+apt-get install mariadb-server
+apt-get install software-properties-common
+apt-get install libapache2-mod-php7.0
