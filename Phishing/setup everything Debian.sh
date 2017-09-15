37d0bd9aad4309b3a3f4c770100900d26bed08e1
jean-brice <jdickewu@gmail.com>
2017-09-15 16:57:32 +0000

    modified the script to install necessary files by adding an if statement refs #7

--- Phishing/setup everything Debian.sh
+++ Phishing/setup everything Debian.sh
@@ -1,9 +1,16 @@
 #! bin/bash 
 
 echo "don't forget to run as root"
-#update 
-apt-get update 
-apt-get install apache2
-apt-get install mariadb-server
-apt-get install software-properties-common
-apt-get install libapache2-mod-php7.0
+echo "those will be installed 
+    apache 2 , maria db server, software properties common
+    and libapache2-mod-php7.0
+       are you ok ? (y/n)"
+#apt-get update 
+read answ
+
+if [[ $answ == "y" ]]; then 
+    apt-get install apache2
+    apt-get install mariadb-server
+    apt-get install software-properties-common
+    apt-get install libapache2-mod-php7.0
+
