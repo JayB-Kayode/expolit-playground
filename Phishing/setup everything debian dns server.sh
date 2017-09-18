8b4e5be6b236c78218cc61cad082098bba32a7df
jean-brice <jdickewu@gmail.com>
2017-09-18 11:01:48 +0000

    added a new line on the script to install mysql server on the DNS Server refs #7

--- Phishing/setup everything debian dns server.sh
+++ Phishing/setup everything debian dns server.sh
@@ -4,12 +4,13 @@
 #install files for dns server on debian 
 echo "don't forget to run as root"
 echo "those will be installed 
-    power DNS server and power DNS 
+    power DNS server and power DNS MySql
        are you ok ? (y/n)"
 #apt-get update 
 read answ
 
 if [[ $answ == "y" ]]; then 
+    apt-get install mysql-server
     apt-get install pdns-server
     apt-get install pdns-backend-mysql
     
